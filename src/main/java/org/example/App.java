d614ab23fd25f203d31297ba8bcfe987efa1e1ad
Alastorial <alex.babykin2003@yandex.ru>
2023-03-16 22:20:31 +0300

    .

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,11 +2,13 @@ package org.example;
 
 import org.example.model.Item;
 import org.example.model.Person;
+import org.hibernate.PersistentObjectException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.SessionFactoryObserver;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,26 +24,20 @@ public class App {
         try {
             session.beginTransaction();
 
-//            Person person = session.get(Person.class, 2);
-//            System.out.println(person);
-//
-//            List<Item> items = person.getItems();
-//
-//            System.out.println(items);
+            Person person = session.get(Person.class, 3);
 
-//            Item item = session.get(Item.class, 5);
-//            System.out.println(item);
-//
-//            System.out.println(item.getOwner());
+            Item item = session.get(Item.class, 3);
+            // удаляем у старого владельца данный товар
+            item.getOwner().getItems().remove(item);
+            // устанавливаем товару нового владельца
+            item.setOwner(person);
+            // новому владельцу добавляем новый товар в список его товаров
+            person.getItems().add(item);
 
-//            Person person = session.get(Person.class, 2);
-//            Item newItem = new Item("Item from Hibernate", person);
-//            session.persist(newItem);
 
 
             session.getTransaction().commit();
 
-//            System.out.println(person1.getId());
         } finally {
             sessionFactory.close();
 
@@ -71,5 +67,53 @@ public class App {
 //                System.out.println(p);
 //                p.setName("Test");
 //            }
+
+        // получение человека и вывод его товаров
+//            Person person = session.get(Person.class, 2);
+//            System.out.println(person);
+//
+//            List<Item> items = person.getItems();
+//
+//            System.out.println(items);
+
+        // получение товара и вывод владельца
+//            Item item = session.get(Item.class, 5);
+//            System.out.println(item);
+//
+//            System.out.println(item.getOwner());
+
+        // получение человека и добавление ему товара
+//            Person person = session.get(Person.class, 2);
+//            Item newItem = new Item("Item from Hibernate", person);
+//            person.getItems().add(newItem);  // задали отношение человеку тоже, двухсторонняя связь
+//            session.persist(newItem);
+
+
+        // создание человека и его товара
+//            Person person = new Person("New person", 32);
+//            Item item = new Item("laptop", person);
+//            person.setItems(List.of(item));
+//
+//            session.persist(person);
+//            session.persist(item);
+
+        // удаление товаров человека
+//            Person person = session.get(Person.class, 3);
+//            List<Item> items = person.getItems();
+//
+//            // SQL
+//            for (Item item : items) {
+//                session.remove(item);
+//            }
+//            // двухсторонняя связь, необходимо для того, чтобы в кэше все было верно
+//            person.getItems().clear();
+
+        // удаление человека
+//            Person person = session.get(Person.class, 2);
+//
+//            session.remove(person);
+//
+//            // Для правильного состояния кеша (обновляем данные, не дожидаясь рефреша хибернейта)
+//            person.getItems().forEach(i -> i.setOwner(null));
     }
 }
